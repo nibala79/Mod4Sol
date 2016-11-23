1b28db6ab39ad3a3cf9e1754f92b5512aa660db1
Nithya Srinivasan <Nithya Srinivasan>
2016-11-23 01:36:34 -0800

    Assignment 4

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,13 +1,15 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
-// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 (function (window){
+
+	// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+	var speakWord = "Hello";
+
 	var helloSpeaker = {};
 	helloSpeaker.speak = function (name){
 	console.log(speakWord + " " + name);	
